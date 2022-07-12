d0acf32ba0a384b0d88286c25b35b0d86fcc067d
Muniba Ahmed <mahmed93@berkeley.edu>
2022-07-11 20:17:55 -0700

    server running

--- api/server.js
+++ api/server.js
@@ -1,3 +1,18 @@
+//IMPORTS ON TOP
+const Users = require("./users/model");
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require("express");
+
+const server = express();
+
+//GLOBAL MIDDLEWARE
+server.use(express.json());
+
+//ENDPOINTS
+
+server.get("/", (req, res) => {
+  res.json({ jaan: "marlo" });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
